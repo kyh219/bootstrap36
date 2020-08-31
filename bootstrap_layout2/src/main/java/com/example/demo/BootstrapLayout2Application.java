eb7c0434de2bd398225e183bd566863f5f35d4db
kyh219 <219kimyh@nvaer.com>
2020-08-31 16:30:43 +0900

    [gittest] first commit and push

---
+++ bootstrap_layout2/src/main/java/com/example/demo/BootstrapLayout2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapLayout2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapLayout2Application.class, args);
+	}
+
+}
